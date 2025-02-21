f34b94c0f340d3858bb8eb84ae68dd91bc28118c
Cerecero <ale.rey.cer@outlook.com>
2025-02-21 11:12:01 -0600

    added a space

--- json.go
+++ json.go
@@ -29,6 +29,6 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	w.WriteHeader(code)
 	_, err = w.Write(dat)
 	if err != nil {
-		log.Fatalf("%s:",err)
+		log.Fatalf("%s:", err)
 	}
 }
